2397a085ca413619b4701df08a1bc11600787133
Vaeth <fyrexbright@gmail.com>
2020-08-31 16:09:25 -0500

    Part 1 : Non Requirments

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform objectToFollow = null;
+
+    Vector3 objectOffset;
+
+    private void Awake()
+    {
+        objectOffset = this.transform.position - objectToFollow.position;
+
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = objectToFollow.position + objectOffset;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 12f;
+    [SerializeField] float turnSpeed = 3f;
+
+    Rigidbody rb = null;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveShip();
+        TurnShip();
+    }
+
+    void MoveShip()
+    {
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * moveSpeed;
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+        rb.AddForce(moveDirection);
+    }
+    
+    void TurnShip()
+    {
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * turnSpeed;
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+        rb.MoveRotation(rb.rotation * turnOffset);
+    }
+
+    public void Kill()
+    {
+        Debug.Log("Player has been killed!");
+        this.gameObject.SetActive(false);
+    }
+
+}
