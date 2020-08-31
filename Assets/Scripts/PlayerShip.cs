5550edde4e47a7008e47286e8c6c32b030b27c5a
Vaeth <fyrexbright@gmail.com>
2020-08-31 17:29:13 -0500

    Part 1: Requirements Done

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -5,29 +5,90 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerShip : MonoBehaviour
 {
+      ParticleSystem ParticleShip1;
+    public ParticleSystem ParticleForward1;
+    public ParticleSystem ParticleForward2;
+
+    public ParticleSystem ParticleBackward1;
+    public ParticleSystem ParticleBackward2;
+
     [SerializeField] float moveSpeed = 12f;
     [SerializeField] float turnSpeed = 3f;
-
+    
     Rigidbody rb = null;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ParticleForward1.Play();
+            ParticleForward2.Play();
+
+            ParticleBackward1.Stop();
+            ParticleBackward2.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ParticleForward1.Stop();
+            ParticleForward2.Stop();
+
+            ParticleBackward1.Play();
+            ParticleBackward2.Play();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        ParticleShip1 = GetComponentInChildren<ParticleSystem>();
     }
 
     private void FixedUpdate()
     {
         MoveShip();
         TurnShip();
+
+ 
+
+
     }
 
     void MoveShip()
     {
         float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * moveSpeed;
+        
         Vector3 moveDirection = transform.forward * moveAmountThisFrame;
         rb.AddForce(moveDirection);
+
+        if(moveAmountThisFrame<1 && moveAmountThisFrame>-1)
+        {
+            ParticleForward1.Stop();
+            ParticleForward2.Stop();
+        }
+
+        /*if (moveAmountThisFrame > 1)
+        {
+            ParticleShip1.Play();
+        }
+        else if (moveAmountThisFrame < -1)
+        {
+            ParticleShip1.Stop();
+        }*/
+
+
+
+
+
     }
-    
+
     void TurnShip()
     {
         float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * turnSpeed;
